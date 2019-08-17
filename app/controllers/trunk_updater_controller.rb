3ae98da087215e6cf6be7d9cef3b435a59dd7ecf
vzvu3k6k <vzvu3k6k@gmail.com>
2019-08-18 05:24:30 +0900

    Update controller

--- app/controllers/trunk_updater_controller.rb
+++ app/controllers/trunk_updater_controller.rb
@@ -2,10 +2,19 @@ class TrunkUpdaterController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def webhook
-    if params[:token] == ENV['REDMINE_TRUNK_UPDATER_TOKEN']
-      head :ok
-    else
-      head :bad_request
+    webhook = TrunkUpdater::Webhook.new(params)
+
+    unless webhook.valid?
+      render status: :bad_request, body: "Can't verify your request"
+      return
     end
+
+    unless webhook.tag.match?(/^r\d+$/)
+      render body: 'Unknown image tag'
+      return
+    end
+
+    TrunkUpdater::SelfRepository.update_container(webhook.tag)
+    render body: "Update to #{webhook.tag}"
   end
 end
