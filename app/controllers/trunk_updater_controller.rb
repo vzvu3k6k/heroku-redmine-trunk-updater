825989ed2c40f70c52029fbb8efa7a4dbf64d3dd
vzvu3k6k <vzvu3k6k@gmail.com>
2019-08-18 05:04:36 +0900

    Check token

--- app/controllers/trunk_updater_controller.rb
+++ app/controllers/trunk_updater_controller.rb
@@ -2,6 +2,10 @@ class TrunkUpdaterController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def webhook
-    head :bad_request
+    if params[:token] == ENV['REDMINE_TRUNK_UPDATER_TOKEN']
+      head :ok
+    else
+      head :bad_request
+    end
   end
 end
--- test/functional/webhook_test.rb
+++ test/functional/webhook_test.rb
@@ -1,8 +1,22 @@
 require_relative '../test_helper'
 
 class TrunkUpdaterControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @original_env = ENV.to_h
+    ENV['REDMINE_TRUNK_UPDATER_TOKEN'] = 'foobar'
+  end
+
+  teardown do
+    ENV.replace(@original_env)
+  end
+
   test 'should return 400 if token is invalid' do
     post update_webhook_path(token: 'INVALID')
     assert_response :bad_request
+  end
+
+  test 'should return 200 if token is valid' do
+    post update_webhook_path(token: 'foobar')
+    assert_response :ok
   end
 end
