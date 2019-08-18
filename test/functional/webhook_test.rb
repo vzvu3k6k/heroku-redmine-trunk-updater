9a9e2f6f781c0efa173c558258e80263c349905c
vzvu3k6k <vzvu3k6k@gmail.com>
2019-08-19 06:06:47 +0900

    Update webhook_test.rb

--- test/functional/webhook_test.rb
+++ test/functional/webhook_test.rb
@@ -10,13 +10,27 @@ class TrunkUpdaterControllerTest < ActionDispatch::IntegrationTest
     ENV.replace(@original_env)
   end
 
-  test 'should return 400 if token is invalid' do
+  test 'when token is invalid' do
+    TrunkUpdater::SelfRepository.expects(:update_image_tag).never
     post update_webhook_path(token: 'INVALID')
     assert_response :bad_request
+    assert_equal "Can't verify your request", response.body
   end
 
-  test 'should return 200 if token is valid' do
-    post update_webhook_path(token: 'foobar')
+  test 'when token is valid but tag is unknown' do
+    TrunkUpdater::SelfRepository.expects(:update_image_tag).never
+    post update_webhook_path(token: 'foobar', push_data: { tag: 'latest' })
     assert_response :ok
+    assert_equal 'Unknown image tag', response.body
+  end
+
+  test 'when token is valid and tag is known' do
+    TrunkUpdater::SelfRepository
+      .expects(:update_image_tag)
+      .with('git@github.com:vzvu3k6k/heroku-redmine-trunk.git', 'r456')
+      .once
+    post update_webhook_path(token: 'foobar', push_data: { tag: 'r456' })
+    assert_response :ok
+    assert_equal 'Update to r456', response.body
   end
 end
