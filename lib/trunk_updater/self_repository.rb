e7460a004a533f33da088d9fd89b7277bd53d7cf
vzvu3k6k <vzvu3k6k@gmail.com>
2019-08-19 03:39:36 +0900

    Inline methods for readability

--- lib/trunk_updater/self_repository.rb
+++ lib/trunk_updater/self_repository.rb
@@ -4,53 +4,39 @@ require 'tmpdir'
 
 module TrunkUpdater
   class SelfRepository
-    # Rake adds sh method to FileUtils.
-    extend FileUtils
-    include FileUtils
+    class << self
+      # Rake adds sh method to FileUtils.
+      include FileUtils
 
-    def self.update_image_tag(repository, tag)
-      Dir.mktmpdir do |tmpdir|
-        repo = clone(repository: repository, directory: tmpdir)
-        repo.update_dockerfile(tag)
-        repo.add_and_commit "Update to #{tag}"
-        repo.push
+      def update_image_tag(repository, tag)
+        Dir.mktmpdir do |dir|
+          sh "git clone --depth 1 #{repository.shellescape} #{dir.shellescape}"
+          update_dockerfile(File.join(dir, 'Dockerfile'), tag)
+
+          chdir dir do
+            sh git_identity_env, "git -C #{dir.shellescape} commit -am #{"Update to #{tag}".shellescape}"
+            sh "git -C #{dir.shellescape} push"
+          end
+        end
       end
-    end
 
-    def self.clone(repository:, directory:)
-      sh "git clone --depth 1 #{repository.shellescape} #{directory.shellescape}"
-      new(directory)
-    end
+      private
 
-    attr_reader :dir
+      def update_dockerfile(dockerfile_path, tag)
+        new_content =
+          File.read(dockerfile_path)
+              .sub(%r{^FROM vzvu3k6k/redmine:.+$}, "FROM vzvu3k6k/redmine:#{tag}")
+        File.write(dockerfile_path, new_content)
+      end
 
-    def initialize(dir)
-      @dir = dir
-    end
-
-    def update_dockerfile(tag)
-      filepath = File.join(dir, 'Dockerfile')
-      new_content =
-        File.read(filepath)
-            .sub(%r{^FROM vzvu3k6k/redmine:.+$}, "FROM vzvu3k6k/redmine:#{tag}")
-      File.write(filepath, new_content)
-    end
-
-    def add_and_commit(message)
-      sh identity_env, "git -C #{dir.shellescape} commit -am #{message.shellescape}"
-    end
-
-    def push
-      sh "git -C #{dir.shellescape} push"
-    end
-
-    def identity_env
-      {
-        'GIT_AUTHOR_NAME' => 'vzvu3k6k (bot)',
-        'GIT_AUTHOR_EMAIL' => 'vzvu3k6k@gmail.com',
-        'GIT_COMMITTER_NAME' => 'vzvu3k6k (bot)',
-        'GIT_COMMITTER_EMAIL' => 'vzvu3k6k@gmail.com',
-      }
+      def git_identity_env
+        {
+          'GIT_AUTHOR_NAME' => 'vzvu3k6k (bot)',
+          'GIT_AUTHOR_EMAIL' => 'vzvu3k6k@gmail.com',
+          'GIT_COMMITTER_NAME' => 'vzvu3k6k (bot)',
+          'GIT_COMMITTER_EMAIL' => 'vzvu3k6k@gmail.com',
+        }
+      end
     end
   end
 end
